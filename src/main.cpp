515ea6a8d29f306f4595a96ec1f246a4b3a6c95a
xr <192319969@qq.com>
2025-04-09 16:07:24 +0800

    [format]

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,6 @@
 #include "LedModule.h"
 #include "LaserModule.h"
 // put function declarations here:
-void printEspId();
 
 // LoRaModule lora;
 // Led led;
@@ -17,40 +16,40 @@ TaskHandle_t laserTaskHandle = NULL;
 // 激光测距任务
 void laserTask(void *pvParameters) {
   // 初始化激光模块
-  laser.begin();
-  laser.sendReadCommand();
-  Serial.println("激光模块初始化完成");
-  
-  // 任务主循环
-  while (1) {
-    int16_t distance = laser.receiveReadResponse();
-    if (distance != -1) {
-      Serial.print("测量的距离: ");
-      Serial.println(distance);
-    }
+    laser.begin();
+    laser.sendReadCommand();
+    Serial.println("激光模块初始化完成");
     
-    // 添加一个小延时，避免过于频繁的读取
-    vTaskDelay(pdMS_TO_TICKS(10));
-  }
+  // 任务主循环
+    while (1) {
+        int16_t distance = laser.receiveReadResponse();
+        if (distance != -1) {
+        //   Serial.print("测量的距离: ");
+            Serial.println(distance); 
+        }
+        
+        // 添加一个小延时，避免过于频繁的读取
+        vTaskDelay(pdMS_TO_TICKS(10));
+    }
 }
 
 void setup() {
-  Serial.begin(115200);
-  Serial.println("系统初始化");
-  
+    Serial.begin(115200);
+    Serial.println("系统初始化");
+    
   // 创建激光测距任务
-  xTaskCreatePinnedToCore(
-    laserTask,           // 任务函数
-    "LaserTask",         // 任务名称
-    10000,               // 堆栈大小
-    NULL,                // 任务参数
-    1,                   // 任务优先级
-    &laserTaskHandle,    // 任务句柄
-    1                    // 运行核心 (1 = 核心1)
-  );
-  
+    xTaskCreatePinnedToCore(
+        laserTask,           // 任务函数
+        "LaserTask",         // 任务名称
+        10000,               // 堆栈大小
+        NULL,                // 任务参数
+        1,                   // 任务优先级
+        &laserTaskHandle,    // 任务句柄
+        1                    // 运行核心 (1 = 核心1)
+    );
+    
   // 删除setup任务，因为不再需要
-  vTaskDelete(NULL);
+    vTaskDelete(NULL);
 }
 
 void loop() {
@@ -59,20 +58,3 @@ void loop() {
 }
 
 // put function definitions here:
-
-void printEspId()
-{
-    // 读取芯片ID
-  uint64_t chipId = ESP.getEfuseMac(); // 获取芯片 MAC 地址（作为芯片ID）
-  
-  // 将64位整数分成两个32位整数以便打印
-  uint32_t chipId_high = (uint32_t)(chipId >> 32);
-  uint32_t chipId_low = (uint32_t)chipId;
-  
-  // 打印芯片ID（十六进制格式）
-  Serial.printf("ESP32 Chip ID = %08X%08X\n", chipId_high, chipId_low);
-  
-  // 也可以分别打印高32位和低32位
-  Serial.printf("High = 0x%08X\n", chipId_high);
-  Serial.printf("Low = 0x%08X\n", chipId_low);
-}
